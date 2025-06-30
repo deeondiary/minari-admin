f0d38cd7726c9d11dd62daa743da1651781e0cd0
Jiwon Choi <jiwon94k@gmail.com>
2025-06-30 15:06:42 +0900

    chore: 프로젝트 환경설정
    - style 파일 추가
    - prettier 설정 파일 추가
    - 폰트 파일 추가

---
+++ .prettierrc.mjs
@@ -0,0 +1,12 @@
+export default {
+  $schema: "https://json.schemastore.org/prettierrc",
+  singleQuote: true,
+  semi: true,
+  tabWidth: 2,
+  endOfLine: 'auto',
+  bracketSameLine: false,
+  bracketSpacing: false,
+  printWidth: 100,
+  trailingComma: 'all',
+};
+
